1c5bfd94384c01fe2e2dddfd55fa67ec82a01829
EvertonSouzaa <everton.s.dev@outlook.com>
2023-11-21 10:19:39 -0300

    att scripts

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const listaAnimais = document.querySelector(".animais-lista");
 const animais = listaAnimais.offsetTop;
 const imgs = document.querySelectorAll("img");
+const img = document.querySelector("img");
 
 let i = 0;
 
@@ -8,4 +9,18 @@ imgs.forEach((item, index, array) => {
   // console.log(item);
 });
 
-console.log(animais);
+const linkExterno = document.querySelector('a[href^="http"]');
+
+function handleLinkExterno(event) {
+  event.preventDefault();
+  console.log(this.getAttribute("href"));
+  console.log(event.currentTarget);
+}
+
+function handleKeyboard(event) {
+  if (event.key === "f") {
+    document.body.classList.toggle("azul");
+  }
+}
+
+window.addEventListener("keydown", handleKeyboard);

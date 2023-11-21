f4f0ff0d949778a11598cbf6c175c6a084bd59fb
EvertonSouzaa <everton.s.dev@outlook.com>
2023-11-20 21:36:44 -0300

    scripts

---
+++ script.js
@@ -0,0 +1,11 @@
+const listaAnimais = document.querySelector(".animais-lista");
+const animais = listaAnimais.offsetTop;
+const imgs = document.querySelectorAll("img");
+
+let i = 0;
+
+imgs.forEach((item, index, array) => {
+  // console.log(item);
+});
+
+console.log(animais);
